e58600302dc180fb76e59ece121d314ce4a71511
Sebastian <sebas33696@gmail.com>
2024-07-22 22:21:45 +0200

    agregado boton dark-light

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,19 @@ import Biography from "@/components/Biography/Biography";
 import MyWork from "@/components/MyWork/MyWork";
 import Img from "@/components/Img/Img";
 import { ThemeProvider } from './components/theme-provider';
+import { ModeToggle } from './components/mode-toggle';
 
 
 export default function Home() {
   return (
     <div>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className='flex justify-end'>
+          <ModeToggle />
+
+        </div>
         <div className='m-3 mt-6 flex flex-col justify-center items-center'>
-          <div className= "">
+          <div className="">
             <Img />
           </div>
           <div className="flex flex-col justify-center items-center gap-4 p-4 max-w-4xl">
