f604ceb14ca0ab6bb2459c5e7ee5cfa6a08750a4
Sebastian <sebas33696@gmail.com>
2024-11-20 13:22:56 +0100

    modificada foto de perfil por errores

--- src/components/Img/Img.tsx
+++ src/components/Img/Img.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Img = () => {
   return (
     <div className="flex justify-center max-w-custom mt-3">
-      <img className=" rounded-md" src="https://media.licdn.com/dms/image/D4D03AQHCX-HTVRtTKQ/profile-displayphoto-shrink_400_400/0/1713890039017?e=1726704000&v=beta&t=8hDr1y51Qm6MJS0X4861CLS0NnVxh7Q2_8yfAx2ixec" alt="Foto de perfil" />
+      <img className=" rounded-md" src="/Perfil.jpg" alt="Foto de perfil" />
     </div>
   )
 }
